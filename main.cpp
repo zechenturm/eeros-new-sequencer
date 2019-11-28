6aac328992ebf74babbb4f263a5f19b7190a23ea
LVTTL <contact@lvttl.xyz>
2019-11-28 18:19:30 +0100

    add MySequence

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include "Sequencer.hpp"
 
+class MySequence: public Sequence
+{
+public:
+    std::string text = "hello";
+    void action() override
+    {
+        std::cout << "hello from action\n";
+    }
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
     auto s = Sequencer{};
-    s.constructAndAdd<Sequence>("test");
-    std::cout << s.exists("test");
-    std::cout << s.exists("test2");
+    auto mySequence = s.constructAndAdd<MySequence>("test");
+    std::cout << s.exists("test") << '\n';
+    std::cout << s.exists("test2") << '\n';
+    std::cout << mySequence->text << '\n';
     return 0;
 }
